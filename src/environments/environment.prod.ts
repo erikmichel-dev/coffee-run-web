21170b76cbba984051a942218db9edbe28b657bf
Erik Michel <erikmichelrobado@gmail.com>
2023-12-09 18:37:16 +0100

    project initialized

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl: "https://a0zpgqqrs2.execute-api.eu-south-2.amazonaws.com/prod",
+  apiKey: "G2OP3raBIZ1V9jRNHFPpl1Os0KcAKky14Y3ygpPK"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl: "https://jfrfryrkn3.execute-api.eu-south-2.amazonaws.com/dev",
+  apiKey: "ujx2QP4gygYRk9haZTfz7NnYOXrLmE56etcCagKi"
+};
